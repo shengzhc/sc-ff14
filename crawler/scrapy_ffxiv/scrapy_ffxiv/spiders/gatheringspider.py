39941a6f6511b5fe00c7d204887817ce43558f3c
Shengzhe Chen <shengzhe.chen@gmail.com>
2021-01-03 22:53:53 -0800

    [spider]download the gathering html page

--- crawler/scrapy_ffxiv/scrapy_ffxiv/spiders/gatheringspider.py
+++ crawler/scrapy_ffxiv/scrapy_ffxiv/spiders/gatheringspider.py
@@ -1,8 +1,20 @@
 import scrapy
 
 """
+
 Spider to gather info from `www.ffxiv-gathering.com`
+
 """
 
 class GatheringSpider(scrapy.Spider):
-	name = "ffxiv-gathering"
+	name = 'ffxiv-gathering'
+	allowed_domain = 'ffxiv-gathering.com'
+	start_urls = [
+		'https://www.ffxiv-gathering.com/all.php',
+	]
+
+	def parse(self, response):
+		page = response.url.split("/")[-2]
+        filename = f'downloads/gathering-{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
