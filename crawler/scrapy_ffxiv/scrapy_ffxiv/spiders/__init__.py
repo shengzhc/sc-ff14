ed7e759dddec9956ce0a3d61637a69dd6ea812c4
Shengzhe Chen <shengzhe.chen@gmail.com>
2021-01-05 08:31:36 -0800

    [spider]remove log file when launching from spider

--- crawler/scrapy_ffxiv/scrapy_ffxiv/spiders/__init__.py
+++ crawler/scrapy_ffxiv/scrapy_ffxiv/spiders/__init__.py
@@ -2,3 +2,9 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
+
+
+# Remove the last log file if exists
+import os
+if os.path.exists("latest.log"):
+	os.remove("latest.log")
