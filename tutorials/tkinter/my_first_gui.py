9727c5c997ad93708ee4131ad609ff416647d998
Shengzhe Chen <shengzhe.chen@gmail.com>
2021-01-06 08:12:37 -0800

    [tkinter]use grid space manager

--- tutorials/tkinter/my_first_gui.py
+++ tutorials/tkinter/my_first_gui.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Label, Button
+from tkinter import Tk, Label, Button, W
 
 
 class MyFirstGUI:
@@ -6,14 +6,14 @@ class MyFirstGUI:
 		self.master = master
 		self.master.title('A simple GUI')
 
-		self.label = Label(master, text='This is our first GUI!')
-		self.label.pack()
+		self.label = Label(master, text='This is our first GUI!  - A long long long long text')
+		self.label.grid(columnspan=2, sticky=W)
 
 		self.greet_btn = Button(master, text='Greet', command=self.greet)
-		self.greet_btn.pack()
+		self.greet_btn.grid(row=1)
 
 		self.close_btn = Button(master, text='Close', command=master.quit)
-		self.close_btn.pack()
+		self.close_btn.grid(row=1, column=1)
 
 	def greet(self):
 		print('Greetings!')
