489982bfaa63385d4cc0e28d6c802898425783ff
Shengzhe Chen <shengzhe.chen@gmail.com>
2021-01-11 23:21:03 -0800

    [ff14]refine the fishing data xpath

--- crawler/scrapy_ffxiv/spiders/carbuncleplushy_spider.py
+++ crawler/scrapy_ffxiv/spiders/carbuncleplushy_spider.py
@@ -1,5 +1,7 @@
 import scrapy
 import requests
+import js2py
+
 
 class CarbuncleplushySpider(scrapy.Spider):
     _carbuncleplushy_base_url = 'https://ff14fish.carbuncleplushy.com'
@@ -9,11 +11,12 @@ class CarbuncleplushySpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        src_list = response.selector.xpath("//head/script[contains(@src, 'data.js')]/@src").getall()
+        src_list = response.selector.xpath("//head/script[contains(@src, 'data.js') and not(contains(@src, 'fish_info_data'))]/@src").getall()
         for src in src_list:
             data_url = requests.compat.urljoin(self._carbuncleplushy_base_url, src)
-            yield scrapy.Request(data_url, self.parse_data)
+            yield scrapy.Request(data_url, self.parse_fishing_data)
 
-    def parse_data(self, response):
+    def parse_fishing_data(self, response):
         # next diff to parse the js file
+        ret = js2py.eval_js(response.text).to_dict()
         breakpoint()
