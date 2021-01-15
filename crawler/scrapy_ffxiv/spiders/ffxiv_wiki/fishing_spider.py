067ec859efb80e3f2c5fc74cc0e4c09a7896b393
Shengzhe Chen <shengzhe.chen@gmail.com>
2021-01-14 21:37:04 -0800

    [ff14]set up wiki fishing spider

---
+++ crawler/scrapy_ffxiv/spiders/ffxiv_wiki/fishing_spider.py
@@ -0,0 +1,23 @@
+import scrapy
+
+
+class fishing_spider(scrapy.Spider):
+
+    """
+    This spider scraps on 4 main fishing locations page and further
+    scraps the fish page.
+    """
+
+    name = "ffxiv_wiki_fishing_spider"
+    allowed_domains = [
+        "ffxiv.consolegameswiki.com",
+    ]
+    start_urls = [
+        "https://ffxiv.consolegameswiki.com/wiki/Fishing_Locations",
+        "https://ffxiv.consolegameswiki.com/wiki/Heavensward_Fishing_Locations",
+        "https://ffxiv.consolegameswiki.com/wiki/Stormblood_Fishing_Locations",
+        "https://ffxiv.consolegameswiki.com/wiki/Shadowbringers_Fishing_Locations",
+    ]
+
+    def parse(self, response):
+        pass
