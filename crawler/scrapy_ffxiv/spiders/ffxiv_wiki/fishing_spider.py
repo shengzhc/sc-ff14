38a50ecde737174af13f1bd59715efb6078639ab
Shengzhe Chen <shengzhe.chen@gmail.com>
2021-01-15 23:26:58 -0800

    [ff14]parse fish column

--- crawler/scrapy_ffxiv/spiders/ffxiv_wiki/fishing_spider.py
+++ crawler/scrapy_ffxiv/spiders/ffxiv_wiki/fishing_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+import requests
 
 
 class fishing_spider(scrapy.Spider):
@@ -19,7 +20,21 @@ class fishing_spider(scrapy.Spider):
         "https://ffxiv.consolegameswiki.com/wiki/Shadowbringers_Fishing_Locations",
     ]
 
+    site_base = "https://ffxiv.consolegameswiki.com/"
+    fish_column = {
+        "https://ffxiv.consolegameswiki.com/wiki/Fishing_Locations": 6,
+        "https://ffxiv.consolegameswiki.com/wiki/Heavensward_Fishing_Locations": 4,
+        "https://ffxiv.consolegameswiki.com/wiki/Stormblood_Fishing_Locations": 4,
+        "https://ffxiv.consolegameswiki.com/wiki/Shadowbringers_Fishing_Locations": 4,
+    }
+
     def parse(self, response):
-        rows = response.selector.xpath("//table[contains(@class, 'gathering-role')]//tr[position()>1]//td/..").getall()
+        rows = response.selector.xpath("//table[contains(@class, 'gathering-role')]//tr[position()>1]//td/..")
         for row in rows:
-            breakpoint()
+            col = self.fish_column[response.url]
+            name = row.xpath(f"td[{col}]/a/text()").get()
+            follow_url = requests.compat.urljoin(self.site_base, row.xpath(f"td[{col}]/a/@href").get())
+            yield response.follow(follow_url, self.parse_fish_page, cb_kwargs=dict(name=name))
+
+    def parse_fish_page(self, response, name):
+        breakpoint()
